3f894033f21f12d319b1f8eef3698b27ec479669
Ratkai Adam <adam.ratkai1@gmail.com>
2026-02-19 16:36:17 +0100

    init places service

--- backend/services/location-service/src/main/java/com/travello/locationservice/LocationServiceApplication.java
+++ backend/services/location-service/src/main/java/com/travello/locationservice/LocationServiceApplication.java
@@ -1,7 +1,10 @@
 package com.travello.locationservice;
 
+import tools.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class LocationServiceApplication {
@@ -10,4 +13,13 @@ public class LocationServiceApplication {
         SpringApplication.run(LocationServiceApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
 }
