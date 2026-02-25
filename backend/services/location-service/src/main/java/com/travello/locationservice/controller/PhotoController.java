921fa8f0cd2245cf670a4b988a28cd924bed198d
Ratkai Adam <adam.ratkai1@gmail.com>
2026-02-25 13:25:16 +0100

    add Swagger to the photo endpoint

--- backend/services/location-service/src/main/java/com/travello/locationservice/controller/PhotoController.java
+++ backend/services/location-service/src/main/java/com/travello/locationservice/controller/PhotoController.java
@@ -1,6 +1,14 @@
 package com.travello.locationservice.controller;
 
+import com.travello.locationservice.dto.PlacePhotoDTO;
 import com.travello.locationservice.service.PlaceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.UUID;
 
+@Tag(name="Photos", description = "Location photos endpoints")
 @RestController
 @RequestMapping("location/photos")
 public class PhotoController {
@@ -21,6 +30,12 @@ public class PhotoController {
         this.placeService = placeService;
     }
 
+    @Operation(summary = "Get photo by place ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Photo received succesfully",
+            content = @Content(array = @ArraySchema(schema = @Schema(type = "string", format = "byte")))),
+            @ApiResponse(responseCode = "404", description = "Photo not found",content = @Content)
+    })
     @GetMapping("/{photoId}")
     public ResponseEntity<byte[]> getPhoto(@PathVariable UUID photoId) {
         byte[] imageBytes = placeService.getPhotoById(photoId);
@@ -32,6 +47,12 @@ public class PhotoController {
                 .body(imageBytes);
     }
 
+    @Operation(summary = "Get all the photo ID by place ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Place photo ids received sucessfully",
+            content = @Content(array = @ArraySchema(schema = @Schema(type = "string", format = "uuid")))),
+            @ApiResponse(responseCode = "404", description = "Photo ids not found", content = @Content)
+    })
     @GetMapping("/all/{placeId}")
     public List<UUID> getIdsFromPhotos(@PathVariable String placeId) {
         return placeService.getPhotoIdsByPlaceId(placeId);
